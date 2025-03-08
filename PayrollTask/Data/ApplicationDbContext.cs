6c45ad8c914519b1a57a5ebcb8ffa78bdc4495ae
Omkar Kajale <omkar.kajale@nimapinfotech.com>
2025-03-08 14:51:32 +0530

    db context and connection string

---
+++ PayrollTask/Data/ApplicationDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PayrollTask.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options){}
+}
