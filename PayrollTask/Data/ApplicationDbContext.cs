0a1c7708e3629de41c8074e306620ab9c1698fe7
PintyaPintya <omkajale09@gmail.com>
2025-03-14 11:24:18 +0530

    added task status seed data

--- PayrollTask/Data/ApplicationDbContext.cs
+++ PayrollTask/Data/ApplicationDbContext.cs
@@ -10,4 +10,17 @@ public class ApplicationDbContext : DbContext
     public DbSet<Employee> Employees { get; set; }
     public DbSet<PTask> PTasks { get; set; }
     public DbSet<PTaskStatus> PTaskStatuses { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<PTaskStatus>().HasData(
+            new PTaskStatus { Id = 1, Name = "Assigned" },
+            new PTaskStatus { Id = 2, Name = "Submitted" },
+            new PTaskStatus { Id = 3, Name = "Under Review" },
+            new PTaskStatus { Id = 4, Name = "Approved" },
+            new PTaskStatus { Id = 5, Name = "Rejected" }
+        );
+
+        base.OnModelCreating(modelBuilder);
+    }
 }
